9ddce58f5a1d0dfabc867c44b1bf89ab2a3474d5
Lucas Silva <ldsilva.sp2002+github@gmail.com>
2023-08-18 14:53:20 -0300

    My Pwsh Codes

---
+++ Compare-Hash.ps1
@@ -0,0 +1,72 @@
+﻿#!/usr/bin/env pwsh
+
+<#
+.SYNOPSIS
+Este programa compara hashs de arquivos com um hash passado como parâmetro.
+
+.DESCRIPTION
+Este programa compara hashs de arquivos com um hash passado como parâmetro.
+
+.PARAMETER FilePath
+Caminho completo do arquivo que será verficado.
+
+.PARAMETER Algorithm
+O algoritmo que será usado para calcular o hash.
+Hashs: md5, sha1, sha256, sha384, sha512
+
+.PARAMETER Hash
+O hash que será comparado com o hash do arquivo.
+
+.EXAMPLE
+./Compare-Hash.ps1 -FilePath "./Caminho/para/o/arquivo" -Algorithm md5 -Hash "C2EB69D59922122FFE105F1336FEA057"
+
+.NOTES
+Nome do Arquivo: Compare-Hash.ps1
+Autor: Lucas Silva
+Versão: 1.0
+
+#>
+
+param(
+    [Parameter(Mandatory = $false)]
+    [string]$FilePath,
+    [Parameter (Mandatory = $false)]
+    [string]$Hash,
+    [Parameter (Mandatory = $false)]
+    [string]$Algorithm,
+    [switch]$help
+)
+
+
+function compare_hash {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$FilePath,
+        [Parameter(Mandatory = $true)]
+        [string]$Hash_to_compare,
+        [Parameter(Mandatory = $true)]
+        [string]$Algorithm
+    )
+    $hash_file = Get-FileHash $FilePath -Algorithm $Algorithm
+    $hash_file = $hash_file.Hash
+    if ($hash_file -eq $Hash_to_compare) {
+        <# Action to perform if the condition is true #>
+        Write-Host "As hashs são iguais. O arquivo não foi alterado." -ForegroundColor Green
+    }
+    else {
+        <# Action when all if and elseif conditions are false #>
+        Write-Host "As hash são diferentes. Provavelmente o arquivo foi alterado." -ForegroundColor Red
+    }
+}
+
+if ($help) {
+    Get-Help -Name $MyInvocation.MyCommand.Definition
+    exit
+}
+elseif (-not $FilePath -or -not $Hash -or -not $Algorithm) {
+    <# Action when this condition is true #>
+    Write-Host "Parâmetros insuficientes. Use -help para obter ajuda." -ForegroundColor Red
+    Get-Help -Name $MyInvocation.MyCommand.Definition
+    exit
+}
+else { compare_hash -FilePath $FilePath -Hash_to_compare $Hash -Algorithm $Algorithm }
---
+++ powercath.ps1
@@ -0,0 +1,145 @@
+﻿#!/usr/bin/env pwsh
+
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$url,
+    [Parameter(Mandatory = $false)]
+    [switch]$getcode,
+    [Parameter(Mandatory = $false)]
+    [switch]$text,
+    [Parameter(Mandatory = $false)]
+    [string]$HeadersFile,
+    [Parameter(Mandatory = $false)]
+    [string[]]$header
+    
+)
+
+
+function parse_headers {
+    param(
+        [Parameter(Mandatory = $false)]
+        [string]$filepath,
+        [Parameter(Mandatory = $false)]
+        [string[]]$args_headers
+    )
+    $req_headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+        
+    if ($filepath -ne '' -and (Test-Path $filepath)) {
+        $file_lines = Get-Content $filepath
+        foreach ($line in $file_lines) {
+            if ($line -match ":") {
+                $key = $line.Substring(0, $line.IndexOf(":"))
+                $value = $line.Substring($line.IndexOf(":") + 1).Trim()
+                $req_headers.Add($key, $value)
+            }
+        }
+    }
+    elseif ($args_headers -ne '' -and ($args_headers.Length -gt 0)) {
+        foreach ($line in $args_headers) {
+            if ($line -match ":") {
+                $key = $line.Substring(0, $line.IndexOf(":"))
+                $value = $line.Substring($line.IndexOf(":") + 1).Trim()
+                $req_headers.Add($key, $value)
+            }
+        }
+    }
+    return $req_headers
+
+}
+
+
+function print_status_code {
+    
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$url,
+        [Parameter(Mandatory = $true)]
+        [int]$code
+    
+    )
+    
+    
+    
+    Write-Host "$url " -NoNewline 
+    
+    if ($code -ige 200 -and ($code -lt 300)) {
+
+        Write-Host "<$code>" -ForegroundColor Green
+
+    }
+    elseif ($code -ige 300 -and ($code -lt 400)) {
+    
+        Write-Host "<$code>" -ForegroundColor Yellow
+
+    }
+    elseif ($code -ige 400 -and ($code -lt 500)) {
+
+        Write-Host "<$code>" -ForegroundColor Red
+
+    }
+
+}
+
+
+function request {
+    
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$url,
+        [Parameter(Mandatory = $false)]
+        $headers
+    )
+    
+    
+    try {
+   
+        $response = Invoke-WebRequest -Uri $url -Headers $headers 
+
+
+        $code = $response.StatusCode
+    }
+    catch {
+        $erro = $_.Exception.Message
+        if ($erro -like "*404*") {
+            $code = 404
+        }
+        elseif ($erro -like "*403*") {
+            $code = 403
+        }
+        elseif ($url -notmatch "http(s)?://") {
+            Write-Host "Valid URL must start with http:// or https://." -ForegroundColor Red
+            exit
+        }
+        else {
+            Write-Host $_.Exception.Message -ForegroundColor Red
+            exit
+        }
+
+    }
+    return $code, $response
+
+}
+
+
+if ($HeadersFile -ne '') {
+    $headers = parse_headers -filepath $HeadersFile -args_headers $header
+    $status_code, $html = request -url $url -headers $headers
+}
+elseif ($null -ne $header) {
+    $headers = parse_headers -args_headers $header
+    $status_code, $html = request -url $url -headers $headers
+} 
+
+else {
+    $status_code, $html = request -url $url
+}
+
+if ($getcode) {
+    print_status_code -url $url -code $status_code
+}
+elseif ($text) {
+    Write-Host $html
+}
+
+
+#Write-Host "`n`n`n $response"
